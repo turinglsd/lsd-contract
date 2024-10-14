4f194599cdb4e791d52b65bc8f411d7ba3316dcf
nick199910 <2413810931@qq.com>
2024-10-14 20:22:47 +0800

    deploy withdraw contract

--- script/DeployToken.s.sol
+++ script/DeployToken.s.sol
@@ -2,16 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {TuNEO, MERC20} from "src/tuNEO.sol";
-import {WtuNEO, ERC20} from "src/wtuNEO.sol";
-import {NativeMinterRedeem, ERC20MinterRedeem} from "src/Minter.sol";
+import {TuNEO, MERC20} from "src/tokens/tuNEO.sol";
+import {WtuNEO, ERC20} from "src/staking/wtuNEO.sol";
+import {NativeMinterRedeem, ERC20MinterRedeem, ERC20MinterWithdrawal} from "src/minters/Minter.sol";
 
 contract DeployScript is Script {
 
     TuNEO tuNEO;
     WtuNEO wtuNEO;
-    NativeMinterRedeem nativeMinter;
-    ERC20MinterRedeem eRC20Minter;
+    // NativeMinterRedeem nativeMinter;
+    // ERC20MinterRedeem eRC20Minter;
+    ERC20MinterWithdrawal erc20Withdraw;
 
     function setUp() public {}
 
@@ -24,11 +25,12 @@ contract DeployScript is Script {
         tuNEO = new TuNEO("tuNEO", "tuNEO", decimals);
         wtuNEO = new WtuNEO(ERC20(address(tuNEO)), rewardsCycleLength);
         tuNEO.mint(address(wtuNEO), 1);
-        nativeMinter = new NativeMinterRedeem(address(tuNEO));
+        // nativeMinter = new NativeMinterRedeem(address(tuNEO));
         MERC20 erc20 = new MERC20("Mock", "Mock", decimals);
         erc20.mint(address(deployer), 1000 * 10 ** 18);
-        eRC20Minter = new ERC20MinterRedeem(address(erc20), address(tuNEO));
-        tuNEO.transferOwnership(address(eRC20Minter));
+        // eRC20Minter = new ERC20MinterRedeem(address(erc20), address(tuNEO));
+        erc20Withdraw = new ERC20MinterWithdrawal(address(erc20), address(tuNEO), "wd", "wd");
+        tuNEO.transferOwnership(address(erc20Withdraw));
         vm.stopBroadcast();
     }
 }
--- test/TestToken.t.sol
+++ test/TestToken.t.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol"; 
-import {TuNEO, MERC20} from "src/tuNEO.sol";
-import {WtuNEO, ERC20} from "src/wtuNEO.sol";
-import {NativeMinterRedeem, ERC20MinterRedeem} from "src/Minter.sol";
+import {TuNEO, MERC20} from "src/tokens/tuNEO.sol";
+import {WtuNEO, ERC20} from "src/staking/wtuNEO.sol";
+import {NativeMinterRedeem, ERC20MinterRedeem} from "src/minters/Minter.sol";
 
 contract TokenTest is Test {
     address owner = address(0x01);
