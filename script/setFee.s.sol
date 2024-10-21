e344411def21f0a6988b18b7cf2bc4f9333e4d05
nick199910 <2413810931@qq.com>
2024-10-21 18:34:33 +0800

    fix deploy contract script

--- script/setFee.s.sol
+++ script/setFee.s.sol
@@ -4,11 +4,13 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 import {TuNEO, MERC20} from "src/tokens/tuNEO.sol";
 import {WtuNEO, ERC20} from "src/staking/wtuNEO.sol";
-import {NativeMinterWithdrawal} from "src/minters/tuNEOMinter.sol";
+import {NativeMinterWithdrawal, ERC20MinterWithdrawal} from "src/minters/tuNEOMinter.sol";
 
 contract SetFeeScript is Script {
 
     NativeMinterWithdrawal withdrawMinter;
+    ERC20MinterWithdrawal erc20withdraw_bneo;
+    ERC20MinterWithdrawal erc20withdraw_gas;
 
 
     function setUp() public {}
@@ -35,10 +37,15 @@ contract SetFeeScript is Script {
         // address deployer = vm.addr(privateKey);
         vm.startBroadcast(privateKey);
         // updateWithdrawalFee
-        withdrawMinter = NativeMinterWithdrawal(0xC6962a4ffeaf4c7fDF46C02ca99C29A4CC366f2e);
-        withdrawMinter.updateWithdrawalFee(50);
-        uint fee = withdrawMinter.withdrawalFee();
-        require(fee == 50, "fee is not 50");
+        // withdrawMinter = NativeMinterWithdrawal(0xC6962a4ffeaf4c7fDF46C02ca99C29A4CC366f2e);
+        erc20withdraw_bneo = ERC20MinterWithdrawal(0xC55215ae37fB289350F26973225f9b1028D5690d);
+        erc20withdraw_gas = ERC20MinterWithdrawal(0xC6962a4ffeaf4c7fDF46C02ca99C29A4CC366f2e);
+
+        // withdrawMinter.updateWithdrawalFee(50);
+        erc20withdraw_bneo.updateWithdrawalFee(50);
+        erc20withdraw_gas.updateWithdrawalFee(50);
+        // uint fee = withdrawMinter.withdrawalFee();
+        // require(fee == 50, "fee is not 50");
         vm.stopBroadcast();
 
     }
